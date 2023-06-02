620e01a078766dbb3d05f0b03e20ad19c88dc11f
alieldeba <alieldeba375@gmail.com>
2023-06-02 12:12:44 +0300

    adding contact page

--- app/page.tsx
+++ app/page.tsx
@@ -52,8 +52,8 @@ export default function Home() {
           pageIndex === 2 ? "block right-0 w-full h-full" : "w-0 h-0 hidden"
         }`}
       >
-        <div className="text-center">
-          <h1 className="text-3xl hidden md:block">Skills</h1>
+        <div className="text-center hidden md:block">
+          <h1 className="text-3xl">Skills</h1>
           <p>All what I have gained in my learning career</p>
         </div>
       </section>
@@ -62,8 +62,8 @@ export default function Home() {
           pageIndex === 3 ? "block right-0 w-full h-full" : "w-0 h-0 hidden"
         }`}
       >
-        <div className="text-center">
-          <h1 className="text-3xl hidden md:block">Projects</h1>
+        <div className="text-center hidden md:block">
+          <h1 className="text-3xl">Projects</h1>
           <p>Best projects that I have made in my career</p>
         </div>
       </section>
@@ -72,32 +72,82 @@ export default function Home() {
           pageIndex === 4 ? "block right-0 w-full h-full" : "w-0 h-0 hidden"
         }`}
       >
-        <div className="text-center">
-          <h1 className="text-3xl hidden md:block">Contact</h1>
+        <div className="text-center hidden md:block">
+          <h1 className="md:text-3xl">Contact</h1>
           <p>All my social media accounts and a way to contact me faster</p>
         </div>
+        <div className="flex flex-col-reverse md:flex-row gap-10 justify-between pt-24 md:pt-32">
+          <div className="flex flex-col items-center gap-5 w-full md:w-[680px]">
+            <h3 className="text-2xl md:text-5xl">
+              Feel free to send me any message I am available 24 hours.
+            </h3>
+            <p>
+              and also you can contact or follow me on those social media
+              accounts.
+            </p>
+            <div className="flex gap-3">
+              <Image
+                src="/linkedin.svg"
+                alt="linkedin"
+                width={50}
+                height={50}
+                className="cursor-pointer"
+              />
+              <Image
+                src="/github.svg"
+                alt="github"
+                width={50}
+                height={50}
+                className="cursor-pointer"
+              />
+              <Image
+                src="/behance.svg"
+                alt="behance"
+                width={50}
+                height={50}
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 w-full md:w-1/2">
+            <label className="font-medium">Email (optional)</label>
+            <input
+              type="email"
+              required
+              className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+            <label className="font-medium">Message</label>
+            <textarea
+              required
+              className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            ></textarea>
+            <button className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150">
+              Send Message
+            </button>
+          </div>
+        </div>
       </section>
-      <div className="flex items-center gap-1 absolute bottom-1 md:bottom-10 left-1/2 -translate-x-1/2 z-10">
+      <div className="flex items-center gap-1 absolute bottom-1 md:bottom-10 left-1/2 -translate-x-1/2 z-10 transition-all">
         <div
-          className={`w-3 h-3 rounded-full ${
+          className={`w-3 h-3 rounded-full transition-all ${
             pageIndex === 1 ? "bg-[#0075FF]" : "bg-[#D8D8D8]"
           }`}
           onClick={() => setPageIndex(1)}
         ></div>
         <div
-          className={`w-3 h-3 rounded-full ${
+          className={`w-3 h-3 rounded-full transition-all ${
             pageIndex === 2 ? "bg-[#0075FF]" : "bg-[#D8D8D8]"
           }`}
           onClick={() => setPageIndex(2)}
         ></div>
         <div
-          className={`w-3 h-3 rounded-full ${
+          className={`w-3 h-3 rounded-full transition-all ${
             pageIndex === 3 ? "bg-[#0075FF]" : "bg-[#D8D8D8]"
           }`}
           onClick={() => setPageIndex(3)}
         ></div>
         <div
-          className={`w-3 h-3 rounded-full ${
+          className={`w-3 h-3 rounded-full transition-all ${
             pageIndex === 4 ? "bg-[#0075FF]" : "bg-[#D8D8D8]"
           }`}
           onClick={() => setPageIndex(4)}
